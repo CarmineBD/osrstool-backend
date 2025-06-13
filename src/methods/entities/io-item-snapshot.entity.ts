52da6b6f59ee54baf16882ebb418cc332196b1ab
Carmine Buonaiuto <carmine-bnt@hotmail.com>
2025-06-14 01:28:44 +0200

    fix: variant_io_item_snapshots id column

--- src/methods/entities/io-item-snapshot.entity.ts
+++ src/methods/entities/io-item-snapshot.entity.ts
@@ -1,21 +1,21 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
+import { Entity, Column, ManyToOne, JoinColumn, PrimaryColumn } from 'typeorm';
 import { VariantSnapshot } from './variant-snapshot.entity';
 
 @Entity('variant_io_items_snapshots')
 export class VariantIoItemSnapshot {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryColumn({ name: 'item_id', type: 'int' })
+  itemId: number;
 
   @ManyToOne(() => VariantSnapshot, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'snapshot_id' })
   snapshot: VariantSnapshot;
 
-  @Column({ name: 'item_id', type: 'int' })
-  itemId: number;
+  @PrimaryColumn({ name: 'snapshot_id', type: 'int' })
+  snapshotId: number;
 
   @Column({ type: 'numeric' })
   quantity: number;
 
-  @Column({ type: 'text' })
+  @PrimaryColumn({ type: 'text' })
   type: 'input' | 'output';
 }
