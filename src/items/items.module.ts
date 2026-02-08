3aee12114985fc2935fc7765db02758c85c5eeb2
CarmineBD <carmine-bnt@hotmail.comgit config --global user.email carmine-bnt@hotmail.com>
2026-02-08 18:29:39 +0100

    feat: add Supabase authentication guards to item management endpoints

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { HttpModule } from '@nestjs/axios';
 import { PricesModule } from '../prices/prices.module';
+import { AuthModule } from '../auth/auth.module';
 import { Item } from './entities/item.entity';
 import { ItemsService } from './items.service';
 import { ItemsController } from './items.controller';
 import { ItemsSeederService } from './items-seeder.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Item]), HttpModule, PricesModule],
+  imports: [TypeOrmModule.forFeature([Item]), HttpModule, PricesModule, AuthModule],
   providers: [ItemsService, ItemsSeederService],
   controllers: [ItemsController],
   exports: [ItemsService, ItemsSeederService],
