15845b59e5807ba20709365a48eb57694a92a8d5
CarmineBD <carmine-bnt@hotmail.comgit config --global user.email carmine-bnt@hotmail.com>
2026-02-08 18:33:45 +0100

    feat: include User entity in TypeOrmModule for ItemsModule

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -4,12 +4,13 @@ import { HttpModule } from '@nestjs/axios';
 import { PricesModule } from '../prices/prices.module';
 import { AuthModule } from '../auth/auth.module';
 import { Item } from './entities/item.entity';
+import { User } from '../auth/entities/user.entity';
 import { ItemsService } from './items.service';
 import { ItemsController } from './items.controller';
 import { ItemsSeederService } from './items-seeder.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Item]), HttpModule, PricesModule, AuthModule],
+  imports: [TypeOrmModule.forFeature([Item, User]), HttpModule, PricesModule, AuthModule],
   providers: [ItemsService, ItemsSeederService],
   controllers: [ItemsController],
   exports: [ItemsService, ItemsSeederService],
